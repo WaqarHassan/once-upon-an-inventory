7a655b3e43111d41c1564f089abf8d0c7f0eecd5
WaqarHassan <whassan25@gmail.com>
2018-06-30 17:24:12 +0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.delete_all
-User.create([{ name: 'Zacharias', email: 'demo@demo.com', password: "change_me", remember_created_at: nil }])
+User.create([{ name: 'waqar', email: 'whassan25@gmail.com', password: "waqar123", remember_created_at: nil }])
