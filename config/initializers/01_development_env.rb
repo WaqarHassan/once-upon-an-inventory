979667b10395e0ba2a34cfc3b2d1c9deceaff9fc
WaqarHassan <whassan25@gmail.com>
2018-07-30 20:25:25 +0500

    environment variables

--- config/initializers/01_development_env.rb
+++ config/initializers/01_development_env.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
   ENV['GOOGLE_CLIENT_ID'] = "..."
   ENV['ANOTHER_VARIABLE'] = "..."
-  ENV['Clinic Name'] = "Liver & Stomach Clinic"
+  ENV['Clinic_Name'] = "Liver & Stomach Clinic"
 end
