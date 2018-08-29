e3fc5fa98a10b8098a2f5d792fef431fff61b5af
WaqarHassan <whassan25@gmail.com>
2018-08-30 00:42:57 +0500

    has_one drugs_stock

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -2,6 +2,6 @@ class Drug < ApplicationRecord
   belongs_to :company
 	has_many :distributor_drugs
 	has_many :distributors, through: :distributor_drugs
-	has_one :drugs_stocks #, class_name: "DrugsStock"
+	has_one :drugs_stock #, class_name: "DrugsStock"
 	validates_presence_of :brand_name , :company_id
 end
