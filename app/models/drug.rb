0336ab68f023f694d940bf2a284b435ce6d8a544
WaqarHassan <whassan25@gmail.com>
2018-08-30 01:04:49 +0500

    normalize_stock

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -4,4 +4,18 @@ class Drug < ApplicationRecord
 	has_many :distributors, through: :distributor_drugs
 	has_one :drugs_stock #, class_name: "DrugsStock"
 	validates_presence_of :brand_name , :company_id
+
+	def self.normalize_stock
+		Drug.all.each do |drug|
+			total_purchase_quantity = DrugsStock.where(drug_id: drug.id).sum(:quantity).to_i
+			total_sold_quantity = InvoiceDrug.where(drug_id: drug.id).sum(:quantity).to_i
+			expected_drug_quantity = total_purchase_quantity - total_sold_quantity
+			ids = []
+			if drug.quantity != expected_drug_quantity
+				ids <<  drug.id
+				# drug.update(quantity: expected_drug_quantity)
+			end
+		end
+		puts ids
+	end
 end
