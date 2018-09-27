fbdb1439587214d2a03d2fca7f595cff4e20e9ee
WaqarHassan <whassan25@gmail.com>
2018-09-28 02:04:24 +0500

    Profit Calculations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,24 @@
 class Invoice < ApplicationRecord
   acts_as_paranoid
   has_many :invoice_drugs, dependent: :destroy
-  def get_net_discount
-
+  def get_profit
+    invoice_drugs.map{|id| (id.get_profit rescue 0.0)}.sum rescue "error"
+  end
+  def get_discount
+    invoice_drugs.map{|id| id.discount - discount}.sum rescue "error"
   end
 end
+
+
+# def get_profit
+#   invoice_drugs.map{|id| (id.quantity * id.price) * ((id.discount - discount )/ 100)}.sum rescue "error"
+#   profit =     0
+#   invoice_drugs.each do |id|
+#     #total_amount = id.quantity * id.price
+#     #invoice_discount_amount= total_amount * ( discount / 100 )  # 5% invoice_discount
+#     #remaining_amount = total_amount- invoice_discount_amount
+#     #profit_discount = id.discount - discount
+#     profit = profit +  id.get_profit
+#   end
+#   return profit
+# end
--- app/models/invoice_drug.rb
+++ app/models/invoice_drug.rb
@@ -5,4 +5,11 @@ class InvoiceDrug < ApplicationRecord
   def amount
     quantity * price rescue nil
   end
+  def get_profit
+    total_amount= quantity * price
+    invoice_discount_amount = total_amount * (invoice.discount / 100)
+    remaining_amount = total_amount - invoice_discount_amount
+    profit_discount  = discount - invoice.discount
+    ((remaining_amount) * (profit_discount / 100)).round(2)
+  end
 end
