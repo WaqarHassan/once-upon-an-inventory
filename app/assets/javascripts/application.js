ea95ef74ee6ff7be97eb88a14b7d404c024bb42f
WaqarHassan <whassan25@gmail.com>
2018-11-07 23:52:42 +0500

    sales report amount total dynamic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery.invoice
+//= require sum
 //= requrie custom
 //= require_tree .
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,6 +7,38 @@ $(document).ready(function(){
     $("#invoices_report,#drugs_stock_report").DataTable({
         "paging":   false,
         "info": false,
+        "footerCallback": function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+
+            // Total over all pages
+            total = api
+                .column( 2 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Total over this page
+            pageTotal = api
+                .column( 2, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Update footer
+            $( api.column( 2 ).footer() ).html(
+                '$'+pageTotal +' ( $'+ total +' total)'
+            );
+        }
     });
     $(".datepicker").datepicker({
         format: "dd/mm/yyyy"
