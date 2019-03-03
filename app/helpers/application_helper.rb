036cf69531a7a9e8f7bb17c7cc5cea9584163a51
WaqarHassan <this.waqar@gmail.com>
2019-03-04 01:04:37 +0500

    discount correction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,24 @@
 module ApplicationHelper
+
   def table_classes
     "table table-bordered table-striped table-hover table-condensed table-bordered table-hover pretty"
   end
+
   def form_title
      controller.action_name.humanize.to_s + " " + controller_name.humanize.singularize rescue ""
   end
+
   def date_format
     '%d %b, %Y'
   end
+
+  def get_customer_discount(invoice_drug)
+    (invoice_drug.amount * (@invoice.discount / 100)).round(2)
+  end
+
+  def get_drug_discount(invoice_drug)
+    (invoice_drug.amount * (invoice_drug.discount / 100)).round(2)
+  end
+
 end
 
